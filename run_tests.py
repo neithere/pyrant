e1f6996454906b57e216e530b642a61f3ae7326e
Andy Mikhailenko <andy@neithere.net>
2009-09-07 07:17:05 +0600

    Added testing suite. Much more tests should be added.

---
+++ run_tests.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python -O
+# -*- coding: utf-8 -*-
+
+
+import os
+import unittest
+import doctest
+
+
+TESTS_DIRS = ('test',)# 'pyrant')
+
+# A sandbox Tyrant instance parametres:
+TYRANT_HOST = '127.0.0.1'
+TYRANT_PORT = '1983'    # default is 1978 so we avoid clashes
+TYRANT_FILE = os.path.abspath('test123.tct')   # NOTE: this file will be purged during testing!
+TYRANT_PID  = os.path.abspath('test123.pid')
+
+
+def _start_tyrant():
+    assert not os.path.exists(TYRANT_FILE), 'Cannot proceed if test database already exists'
+    cmd = 'ttserver -dmn -host %(host)s -port %(port)s -pid %(pid)s %(file)s'
+    os.popen(cmd % {'host': TYRANT_HOST, 'port': TYRANT_PORT,
+                    'pid': TYRANT_PID, 'file': TYRANT_FILE}).read()
+    if __debug__:
+        print 'Sandbox Tyrant started...'
+
+def _stop_tyrant():
+    cmd = 'ps -e -o pid,command | grep "ttserver" | grep "\-port %s"' % TYRANT_PORT
+    line = os.popen(cmd).read()
+    try:
+        pid = int(line.strip().split(' ')[0])
+    except ValueError:
+        'Expected "pid command" format, got %s' % line
+
+    os.popen('kill %s' % pid)
+    if __debug__:
+        print 'Sandbox Tyrant stopped.'
+
+    os.unlink(TYRANT_FILE)
+    if __debug__:
+        print 'Sandbox database %s deleted.' % TYRANT_FILE
+
+def _add_files_to(suite):
+    def _inner(_, dirname, fnames):
+        for f in fnames:
+            if f.endswith('.py'):
+                file_path = os.path.join(dirname, f)
+                suite.addTest(doctest.DocFileSuite(file_path, optionflags=doctest.ELLIPSIS))
+    return _inner
+
+def _test():
+    # run the sandbox Tyrant instance
+    _start_tyrant()
+
+    # set up suite
+    suite = unittest.TestSuite()
+
+    # collect files for testing
+    for directory in TESTS_DIRS:
+        os.path.walk(directory, _add_files_to(suite), None)
+
+    # run tests on all files collected
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
+
+    # kill the sandbox Tyrant instance
+    _stop_tyrant()
+
+if __name__ == '__main__':
+    _test()
--- test/tests.py
+++ test/tests.py
@@ -1,14 +1,96 @@
-"""
-Doctest logic. We keep Tyrant() Q and Tyrant visible for all tests
-"""
-import doctest
-from pyrant import Tyrant, Q
+# -*- coding: utf-8 -*-
 
-t = Tyrant()
-globs = {
-    'Tyrant': Tyrant,
-    'Q': Q,
-    't': t
-}
-doctest.testfile('../pyrant/__init__.py', globs=globs, 
-                 optionflags=doctest.ELLIPSIS)
+class TestBase:
+    """
+    >>> import pyrant
+    >>> from pyrant import Tyrant, Q
+    >>> t = Tyrant(host='127.0.0.1', port=1983)    # default port is 1978
+    >>> t.clear()
+
+    """
+    def basic(self):
+        """
+        >>> if t.dbtype != pyrant.DBTYPETABLE:
+        ...     t['key'] = 'foo'
+        ...     print t['key']
+        ... else:
+        ...     t['key'] = {'name': 'foo'}
+        ...     print t['key']['name']
+        foo
+        >>> del t['key']
+        >>> print t['key']
+        Traceback (most recent call last):
+            ...
+        KeyError: 'key'
+
+        """
+
+    def get_default(self):
+        """
+        >>> t['foo'] = {'a': 'z', 'b': 'y'}
+        >>> print t.get('foo', {})
+        {u'a': u'z', u'b': u'y'}
+        >>> print t.get('bar', {})
+        {}
+
+        """
+
+class TestTable(TestBase):
+    """
+    >>> t.dbtype == pyrant.DBTYPETABLE
+    True
+
+    """
+
+    def query_filter(self):
+        """
+        >>> t['i'] = {'name': 'Reflejo', 'test': 0}
+        >>> t['you'] = {'name': 'Fulano', 'test': 1}
+        >>> res = t.query.filter(Q(name='Reflejo'), Q(test=0))
+        >>> key, data = res[0]
+        >>> key
+        u'i'
+        >>> data['name']
+        u'Reflejo'
+        >>> res = t.query.filter(Q(name='Reflejo') | Q(name='Fulano'))
+        >>> len(res)
+        2
+
+        """
+
+    def query_exclude(self):
+        """
+        >>> q = t.query.exclude(name='Fulano')
+        >>> [x[0] for x in q]
+        [u'foo', u'i']
+
+        """
+
+    def query_order(self):
+        """
+        >>> q = t.query.order('test')
+        >>> [x[0] for x in q]
+        [u'i', u'you', u'foo']
+        >>> q = q.order('-#test')
+        >>> [x[0] for x in q]
+        [u'you', u'i', u'foo']
+
+        """
+
+    def query_values(self):
+        """
+        >>> t.query.values('name')
+        [u'Reflejo', u'Fulano']
+        >>> t.query.values('test')
+        [u'1', u'0']
+        >>> t.query.values('camelot')
+        []
+
+        """
+
+    def query_stat(self):
+        """
+        >>> t.query.stat()
+        {u'a': 1, u'test': 2, u'b': 1, u'name': 2}
+
+        """
