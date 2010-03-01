c43029c197d4256aa210f0c0ae7f7834d70ca4d1
Eivind Uggedal <eivind@uggedal.com>
2010-03-01 14:06:58 +0100

    Python 2.5 compatible exception instantiation.

--- pyrant/query.py
+++ pyrant/query.py
@@ -563,7 +563,7 @@ class Condition(object):
             if definition.accepts(self.expr):
                 try:
                     value = definition.validate(self.expr)
-                except ValueError as e:
+                except ValueError, e:
                     raise ValueError(u'Bad lookup %s__%s=%s: %s' % (
                                      self.name,
                                      self.lookup,
